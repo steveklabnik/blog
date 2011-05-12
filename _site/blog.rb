7763fc866f56882c4ce299d4999f201c82867668
Steve Klabnik <steve@steveklabnik.com>
2011-05-12 15:48:19 -0400

    We need _site, actually.

---
+++ _site/blog.rb
@@ -0,0 +1,24 @@
+require 'sinatra/base'
+
+require_relative "redirects"
+
+class Blog < Sinatra::Base 
+  set :root, File.dirname(__FILE__)
+  set :public, Proc.new { File.join(root, "_site") }
+
+  before do
+    response.headers['Cache-Control'] = 'public, max-age=31557600' # 1 year
+  end
+
+  get '/' do
+    File.read('_site/index.html')
+  end
+
+  REDIRECTS.each do |hsh|
+    get hsh[:from] do
+      redirect hsh[:to], 301 #yeah, this move is permanent
+    end
+  end
+
+end
+
